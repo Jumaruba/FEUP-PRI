0b97f5591d2eca41db212b2e299e29399964a108
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-11-04 11:43:09 +0000

    main: format

--- main.py
+++ main.py
@@ -2,32 +2,40 @@ from bs4 import BeautifulSoup as bs
 import requests
 from core import BookInfo
 
-def get_soup(url):
+
+def get_soup(url) -> bs:
     page = requests.get(url)
     return bs(page.content, 'html.parser')
 
-def get_books(soup): 
-    for a in soup.find_all('a', {'class':'bookTitle'}, href=True):
+
+def get_books(soup) -> str:
+    for a in soup.find_all('a', {'class': 'bookTitle'}, href=True):
         yield a['href']
 
-def one_book(url):
-    book = BookInfo(url)
-    with open('document.csv','a') as fd:
-        fd.write('{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(book.get_book_title(), book.get_book_authors(), \
-            book.get_description(), book.get_number_pages(), book.get_publisher(), book.get_isbn(), book.get_isbn13(), \
-                book.get_rating(), book.get_rating(), book.get_last_date(), book.get_first_date(), book.get_author_url()))
 
-def all_books():
+def one_book(url) -> None:
+    book = BookInfo(url)
+    with open('document.csv', 'a') as fd:
+        fd.write('{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(book.get_book_title(), book.get_book_authors(),
+                                                                book.get_description(), book.get_number_pages(), 
+                                                                book.get_publisher(), book.get_isbn(), book.get_isbn13(),
+                                                                book.get_rating(), book.get_rating(), book.get_last_date(), 
+                                                                book.get_first_date(), book.get_author_url()))
+
+
+def all_books() -> None:
     for i in range(1, 101):
         seed_url = "https://www.goodreads.com/list/show/1.Best_Books_Ever?page={}".format(i)
         soup = get_soup(seed_url)
         for book in get_books(soup):
             one_book("https://www.goodreads.com/" + book)
 
+
 if __name__ == "__main__":
     val_input = input("Type 0 for one book. Another key for all books.\n")
     with open('document.csv', "w") as fd:
-        fd.write('title,authors,description,number_pages,publisher,isbn,isbn13,rating,last_date,first_date,author_url,\n')
+        fd.write(
+            'title,authors,description,number_pages,publisher,isbn,isbn13,rating,last_date,first_date,author_url,\n')
 
     if val_input == "0":
         one_book("https://www.goodreads.com/book/show/2767052-the-hunger-games")
