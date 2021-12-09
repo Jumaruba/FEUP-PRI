8dd32f2c0d2bc57b710ba87d8a107279e9a15744
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-12-09 11:52:50 +0000

    Finally: synonyms working!

--- src/solr_config/init.sh
+++ src/solr_config/init.sh
@@ -11,9 +11,12 @@ sleep 4
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/books/schema
 
+#echo "pc, computer, technology" >> /var/solr/data/books/conf/synonyms.txt
+
 # Populate collection
 bin/post -c authors /data/authors.csv
 bin/post -c books /data/books.csv
 bin/post -c reviews /data/reviews.csv
 
 solr restart -f
+curl 'http://localhost:8983/solr/admin/cores?action=RELOAD&core=books'
