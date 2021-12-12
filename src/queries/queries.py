62f6c630ccd5af77eed02820cc9b75152f2a7483
Diana Freitas <up201806230@fe.up.pt>
2021-12-12 23:06:10 +0000

    Add: save evaluation results

--- src/queries/queries.py
+++ src/queries/queries.py
@@ -1,11 +1,10 @@
 import requests 
-import tematic 
+from metrics import generate_metrics
 
-
-def query_exe(query, file, id_fieldname) -> None:
+def query_exe(query, file, id_fieldname, path) -> None:
     results = requests.get(query).json()['response']['docs']
     relevant = list(map(lambda el: int(el.strip().split(",")[0]), open(file).readlines()))  
-    tematic.generate_metrics(results, relevant, id_fieldname)      
+    generate_metrics(results, relevant, id_fieldname, path)      
 
 # BOOKS ===========================
 BOOKS_QRELS_FILEPATH = "../data/queries/books/related.txt"
@@ -16,19 +15,22 @@ QUERY_BOOKS_2 = "http://localhost:8983/solr/books_subset/select?q=({!q.op=OR df=
 # With genre 
 QUERY_BOOKS_3 = "http://localhost:8983/solr/books_subset/select?q=({!q.op=OR df=description}romantic%26tragedy)^=4&q=title:tragedy%26romantic&rows=10&wt=json" 
 
-#query_exe(QUERY_BOOKS_1, BOOKS_QRELS_FILEPATH, "book_id") 
-#query_exe(QUERY_BOOKS_2, BOOKS_QRELS_FILEPATH, "book_id")   
+#query_exe(QUERY_BOOKS_1, BOOKS_QRELS_FILEPATH, "book_id","tematic/no_boost/") 
+#query_exe(QUERY_BOOKS_2, BOOKS_QRELS_FILEPATH, "book_id","tematic/boost/")   
+#query_exe(QUERY_BOOKS_3, BOOKS_QRELS_FILEPATH, "book_id","tematic/genres/")   
 
 # REVIEWS ==========================  
 REVIEWS_BAD_FILEPATH = "../data/queries/reviews/bad.txt" 
 REVIEWS_GOOD_FILEPATH = "../data/queries/reviews/good.txt" 
 
 bad_search_review = "hated this books"
-# Bad without boost.
+# Bad.
 QUERY_REVIEWS_1 = "http://localhost:8983/solr/reviews_subset/select?q={!q.op=OR df=review_text}hated this books&rows=20&wt=json"
 # With rating and sorted by ascending. 
 QUERY_REVIEWS_2 = "http://localhost:8983/solr/reviews_subset/select?q={!q.op=OR df=review_text}hated this books AND rating:[0 TO 3]&sort=field(rating, min) asc&rows=20&wt=json"
 
+query_exe(QUERY_REVIEWS_1, REVIEWS_BAD_FILEPATH, "review_id", "pos_neg_reviews/synonyms/bad/")
+# query_exe(QUERY_REVIEWS_2, REVIEWS_BAD_FILEPATH, "review_id", "pos_neg_reviews/rating/bad/")
 
 good_search_review = "in love with this book"
 # Good without boost.
@@ -41,4 +43,3 @@ QUERY_REVIEWS_4 = "http://localhost:8983/solr/reviews_subset/select?q={!q.op=OR 
 QUERY_AUTHORS = 'http://localhost:8983/solr/reviews/select?rows=0&q=genres:romance&wt=json&json.facet={top:{ type:terms, limit: 10, field:authors, facet:{mean_rating:"avg(rating)", min_rating: "min(rating)", max_rating: "max(rating)"}, sort:{mean_rating: desc, min_rating: desc, max_rating: desc}}}'
 
 
-query_exe(QUERY_REVIEWS_1, REVIEWS_BAD_FILEPATH, "review_id")
