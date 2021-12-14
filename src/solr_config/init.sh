4b054ab7314a2098facbea9515320007d8d4d427
Diana Freitas <up201806230@fe.up.pt>
2021-12-14 11:50:38 +0000

    Synonyms suck

--- src/solr_config/init.sh
+++ src/solr_config/init.sh
@@ -13,12 +13,13 @@ sleep 4
 #curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/books/schema
 curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/reviews/schema 
 #curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/books_subset/schema 
-curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/reviews_subset/schema 
+#curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema.json http://localhost:8983/solr/reviews_subset/schema 
 #cat /solr_config/synonyms.txt >> /var/solr/data/books/conf/synonyms.txt
 
+cat /solr_config/synonyms.txt >> /var/solr/data/reviews/conf/synonyms_review_index.txt
 cat /solr_config/synonyms.txt >> /var/solr/data/reviews/conf/synonyms.txt
 #cat /solr_config/synonyms.txt >> /var/solr/data/books_subset/conf/synonyms.txt 
-cat /solr_config/synonyms.txt >> /var/solr/data/reviews_subset/conf/synonyms.txt
+#cat /solr_config/synonyms.txt >> /var/solr/data/reviews_subset/conf/synonyms.txt
 
 # SIMPLE SCHEMA ==================
 curl -X POST -H 'Content-type:application/json' --data-binary @/solr_config/schema_simple.json http://localhost:8983/solr/books/schema
