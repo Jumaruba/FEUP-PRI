c23734c432ac1b01e684440a747c817605a950e1
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-12-14 11:14:28 +0000

    Add: new query

--- src/queries/queries.py
+++ src/queries/queries.py
@@ -10,14 +10,12 @@ def query_exe(query, file, id_fieldname) -> None:
 # BOOKS ===========================
 BOOKS_QRELS_FILEPATH = "../data/queries/books/related.txt"
 # Without boost
-QUERY_BOOKS_1 = "http://localhost:8983/solr/books_subset/select?q={!q.op=OR df=description}romantic%26tragedy&q=title:tragedy%26romantic&rows=10&wt=json"
+QUERY_BOOKS_1 = "http://localhost:8983/solr/books_subset/select?q=description:romantic%26tragedy&rows=8&wt=json"
 # With boost
-QUERY_BOOKS_2 = "http://localhost:8983/solr/books_subset/select?q=({!q.op=OR df=description}romantic%26tragedy)^=4&q=title:tragedy%26romantic&rows=10&wt=json" 
-# With genre 
-QUERY_BOOKS_3 = "http://localhost:8983/solr/books_subset/select?q=({!q.op=OR df=description}romantic%26tragedy)^=4&q=title:tragedy%26romantic&rows=10&wt=json" 
+QUERY_BOOKS_2 = "http://localhost:8983/solr/books_subset/query?q=description:%20romantic%20tragedy&q.op=OR&defType=edismax&indent=true&qf=description%5E2&ps=4&rows=8"
 
-#query_exe(QUERY_BOOKS_1, BOOKS_QRELS_FILEPATH, "book_id") 
-#query_exe(QUERY_BOOKS_2, BOOKS_QRELS_FILEPATH, "book_id")   
+query_exe(QUERY_BOOKS_1, BOOKS_QRELS_FILEPATH, "book_id") 
+#query_exe(QUERY_BOOKS_2, BOOKS_QRELS_FILEPATH, "book_id")
 
 # REVIEWS ==========================  
 REVIEWS_BAD_FILEPATH = "../data/queries/reviews/bad.txt" 
@@ -37,8 +35,9 @@ QUERY_REVIEWS_3 = "http://localhost:8983/solr/reviews_subset/select?q={!q.op=OR 
 QUERY_REVIEWS_4 = "http://localhost:8983/solr/reviews_subset/select?q={!q.op=OR df=review_text}in love with this book AND rating:[3 TO *]&sort=field(rating, min) desc&rows=20&wt=json&rows=10&wt=json"
 
 
+
 # GROUP AUTHORS ======================
 QUERY_AUTHORS = 'http://localhost:8983/solr/reviews/select?rows=0&q=genres:romance&wt=json&json.facet={top:{ type:terms, limit: 10, field:authors, facet:{mean_rating:"avg(rating)", min_rating: "min(rating)", max_rating: "max(rating)"}, sort:{mean_rating: desc, min_rating: desc, max_rating: desc}}}'
 
 
-query_exe(QUERY_REVIEWS_1, REVIEWS_BAD_FILEPATH, "review_id")
+#query_exe(QUERY_REVIEWS_1, REVIEWS_BAD_FILEPATH, "review_id")
