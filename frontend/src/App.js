f903a438a2b28d60a07eaf5bae0a2a33e35c07b0
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2021-12-19 20:50:32 +0000

    Add: React with query for books

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 function App() {
-  fetch('http://localhost/solr/books_subset_1/select?q=description:romantic%26tragedy&rows=8&wt=json', {mode: 'cors'})
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+
+    const apiUrl = `http://localhost:80/solr/books_subset_1/select?q=description:romantic%26tragedy&rows=8&wt=json`;
+    fetch(apiUrl, {mode:'cors'})
+      .then((res) => res.json())
+      .then((repos) => {
+        console.log(repos['response']['docs']);
+        setAppState({ loading: false, repos: repos['response']['docs'] });
+      });
+  }, [setAppState]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>Best Books Ever!</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Goodreads Advanced Book Search
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
