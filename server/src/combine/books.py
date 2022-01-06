2863d6c471fe6c3fc2cd6d313d7a58663dde1d0c
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-01-06 01:54:48 +0000

    add: fixed queries

--- server/src/combine/books.py
+++ server/src/combine/books.py
@@ -8,29 +8,42 @@ class BooksCombine:
         self.cursor = cursor
 
     def combine(self):
-        # Combine books with genres in books_genres
-        self.cursor.execute("DROP VIEW IF EXISTS books_genres") 
-        self.cursor.execute('''CREATE VIEW books_genres AS
-                            SELECT book.*, genres.genres as genres
-                            FROM book
-                            JOIN genres ON book.book_id = genres.book_id;''')
+        self.cursor.execute("DROP TABLE IF EXISTS books_combined;") 
+        self.cursor.execute('''CREATE TABLE books_combined (
+                                book_id INTEGER,
+                                title VARCHAR(255),
+                                image_url VARCHAR(255),
+                                num_pages VARCHAR(255),
+                                publisher VARCHAR(255),
+                                date VARCHAR(255),
+                                description VARCHAR(255),
+                                isbn INTEGER,
+                                genres VARCHAR(255),
+                                authors VARCHAR(255),
+                                series VARCHAR(255) DEFAULT '');''')
 
-        print("Combine 1")
-        # Combine books_genres with authors in books_author_name
-        self.cursor.execute("DROP VIEW IF EXISTS books_author_name") 
-        self.cursor.execute('''CREATE VIEW books_author_name AS
-                            SELECT books_genres.book_id, books_genres.title, books_genres.image_url, books_genres.num_pages, books_genres.publisher, books_genres.date, books_genres.description, books_genres.isbn, books_genres.genres, group_concat(author.name, ';') as authors
-                            FROM books_genres
-                            JOIN book_author ON books_genres.book_id = book_author.book_id
-                            JOIN author ON author.author_id = book_author.author_id
-                            GROUP BY books_genres.book_id;''')
+        self.cursor.execute('''
+                    INSERT INTO books_combined(book_id,title,image_url,num_pages,publisher,date,description,isbn,genres,authors) 
+                    SELECT book_id,title,image_url,num_pages,publisher,date,description,isbn,genres,authors
+                    FROM (SELECT books_genres.book_id, books_genres.title, books_genres.image_url, books_genres.num_pages, books_genres.publisher, books_genres.date, books_genres.description, books_genres.isbn, books_genres.genres, group_concat(author.name, ';') as authors
+                    FROM (SELECT book.*, genres.genres as genres
+                    FROM book
+                    JOIN genres ON book.book_id = genres.book_id) books_genres
+                    JOIN book_author ON books_genres.book_id = book_author.book_id
+                    JOIN author ON author.author_id = book_author.author_id
+                    GROUP BY books_genres.book_id) book_author_name;
+                    ''')
 
-        print("Combine 2")
-        self.cursor.execute("DROP TABLE IF EXISTS books_combined") 
-        self.cursor.execute('''CREATE TABLE books_combined AS
-                            SELECT books_author_name.*, group_concat(series.series_name, ';') as series
-                            FROM books_author_name
-                            JOIN book_series ON books_author_name.book_id = book_series.book_id
-                            JOIN series ON series.series_id = book_series.series_id
-                            GROUP BY books_author_name.book_id;''')
+        self.cursor.execute('''
+                    UPDATE books_combined 
+                    SET series = books_combined_series.series
+                    FROM (SELECT book_series.book_id, group_concat(series.series_name, ';') as series
+                    FROM book_series
+                    JOIN series ON series.series_id = book_series.series_id
+                    GROUP BY book_series.book_id) books_combined_series 
+                    WHERE books_combined.book_id = books_combined_series.book_id;''')
+        
         self.connection.commit()
+        
+
+        
--- server/src/combine/reviews.py
+++ server/src/combine/reviews.py
@@ -4,11 +4,9 @@ class ReviewsCombine:
         self.cursor = cursor
 
     def combine(self):
-        print("combine reviews")
         self.cursor.execute("DROP TABLE IF EXISTS reviews_combined") 
         self.cursor.execute('''CREATE TABLE reviews_combined AS
                             SELECT review.review_id, review.book_id, review.rating, review.review_text, review.date_added, books_combined.title, books_combined.genres, books_combined.authors
                             FROM books_combined
                             INNER JOIN review ON books_combined.book_id = review.book_id;''')
         self.connection.commit()
-        print("done reviews")
